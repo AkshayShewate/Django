61426bb1cf2637872f819c38ddbc342db91d770e
AkshayShewate <aksshewate779@gmail.com>
2020-07-27 13:01:16 +0530

    django project added

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\akshay\pycharmprojects\project_django\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
